924e3d5d7140d4af4872f35d33174255af99f0ca
Sdby2003 <115440321+Sdby2003@users.noreply.github.com>
2022-12-06 19:52:21 -0600

    index renewed and Buttons added

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,12 @@
+function findAct(){
 var dogPics = "https://dog.ceo/api/breeds/image/random"
 fetch (dogPics)
     .then(function (response){
         return response.json();
     })
         .then( function (data) {
-            console.log(data.message);
-            var fig = document.querySelector('figure');
-            var img = document.createElement('img');
-            img.src = data.message;
-
-            console.log(fig);
-            fig.appendChild(img);
+            var img = document.getElementById('pic');
+            pic.src = data.message;
         });
 
 var bored = "https://www.boredapi.com/api/activity"
@@ -20,12 +16,9 @@ fetch(bored)
     })
         .then( function (data){
             var activity = data.activity;
-            var type = data.type;
-            var num = data.participants;
-
             var span = document.querySelector('span');
-            span.textContent = activity + type + num;
+            span.textContent = activity + " ";
         });
-
+    }
 var gnBtn = document.getElementById('generate');
-gnBtn.addEventListener('click' );
+gnBtn.addEventListener('click', findAct);
