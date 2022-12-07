522c9db8c19adc51063e48ff3247f23f906bfa45
Sdby2003 <115440321+Sdby2003@users.noreply.github.com>
2022-12-06 21:27:20 -0600

    append saved info

--- assets/script.js
+++ assets/script.js
@@ -20,5 +20,25 @@ fetch(bored)
             span.textContent = activity + " ";
         });
     }
+
 var gnBtn = document.getElementById('generate');
+
 gnBtn.addEventListener('click', findAct);
+
+function saveAct(){
+    var svAct = document.getElementById('activity');
+    localStorage.setItem('activity', svAct.textContent);
+}
+
+var svBtn = document.getElementById('save');
+svBtn.addEventListener('click', saveAct);
+var saves = document.getElementById('savedActs');
+function renderSaves() {
+    var saves = document.getElementById('savedActs');
+    var li = document.createElement('li');
+    li.textContent = localStorage.getItem(activity);
+    saves.appendChild(li);
+}
+
+var sHtml = document.getElementById('saved');
+    sHtml.addEventListener('click', renderSaves);
